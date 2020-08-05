4427e161105244b2e2da3284d0810aa541121966
Adrien Ribeiro <adrienribeiro59200@gmail.com>
2020-08-05 15:39:49 +0200

    Mise à jour code JS page Index
    Rajout de commentaires

--- js/requete.js
+++ js/requete.js
@@ -1,17 +1,13 @@
 const list = document.getElementById("listecam");
 list.className = "menu1CSS";
 
-const getParsedData = function (data) {
-    return JSON.parse(data);
-};
-
 const createElement = function (data) {
     const el = document.createElement("DIV");
     el.classList.add("cam");
     el.className = "menu2CSS";
 
     const link = document.createElement("A");
-    link.href = "/vcam.html?id=" + data._id;
+    link.href = "vcam.html?id=" + data._id;
 
     el.appendChild(link);
 
@@ -42,8 +38,25 @@ const createElement = function (data) {
     return el;
 };
 
-const xhr = new XMLHttpRequest();
+/*
+Test avec demande Fetch
+fetch("http://localhost:3000/api/cameras") 
+    .then(function (reponse) {
+        return response.json()
+    }).then(function (data) {
+        console.log(data)
+    })
+*/
 
+//Requete au serveur 
+const xhr = new XMLHttpRequest();
+/*Fonction avec le statut du serveur
+Si le readystate est done, que le status du serveur est supérieur ou égal à 200 et inférieur à 300
+Alors on créera la constante "data" 
+Pour chaque element de data on va appliquer une fonction "d"
+Nous allons ensuite créer un element avec la méthode "createElement"
+Nous allons créer ensuite une liste ayant pour parent "element".
+*/
 xhr.onreadystatechange = function () {
     if (
         this.readyState == XMLHttpRequest.DONE &&
@@ -60,5 +73,10 @@ xhr.onreadystatechange = function () {
         });
     }
 };
+
+/* La contante "xhr" utilise la méthode ".open" pour une Requete au serveur avec "Get"
+ afin de récuperer les données
+ */
 xhr.open("GET", "http://localhost:3000/api/cameras", true);
-xhr.send();
+//La constante "xhr" nous renvoi le resultat de la requête grâce à méthode "".send"
+xhr.send(); 
