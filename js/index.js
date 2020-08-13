2e60d139f24f233c54da055325cf1a69064189d4
Adrien Ribeiro <adrienribeiro59200@gmail.com>
2020-08-13 18:49:27 +0200

    Maj Graphique
    Resolution du bug du footer
    Changement couleur barre hr

--- js/index.js
+++ js/index.js
@@ -1,91 +1,117 @@
 //Chargement des articles sur le panier dynamiquement
 function chargementPanier(){
-    let nombreProduit = localStorage.getItem('qté'); 
-    
-    if(nombreProduit){
-    document.querySelector ('.totalQté').textContent = nombreProduit;
-    }else{
-        document.querySelector ('.totalQté').textContent = "" ;
-    }
+  let nombreProduit = localStorage.getItem('qté'); 
+  
+  if(nombreProduit){
+  document.querySelector ('.totalQté').textContent = nombreProduit;
+  }else{
+      document.querySelector ('.totalQté').textContent = "" ;
+  }
 }
-  chargementPanier(); 
+chargementPanier(); 
+
+// Ajout des élèments de façon dynamique grâce à la méthode .foreach
 
 const list = document.getElementById("listecam");
 list.className = "menu1CSS";
 
 const getParsedData = function (data) {
-  return JSON.parse(data);
+return JSON.parse(data);
 };
 
 const createElement = function (data) {
-  const el = document.createElement("DIV");
-  el.classList.add("cam");
-  el.className = "menu2CSS";
+const el = document.createElement("DIV");
+el.classList.add("cam");
+el.className = "menu2CSS";
 
-  const link = document.createElement("A");
-  link.href = "produit.html?id=" + data._id;
+const link = document.createElement("A");
+link.href = "produit.html?id=" + data._id;
 
-  el.appendChild(link);
+el.appendChild(link);
 
-  const elChild = document.createElement("DIV");
-  elChild.classList.add("imgContainer");
+const elChild = document.createElement("DIV");
+elChild.classList.add("imgContainer");
 
-  link.appendChild(elChild);
+link.appendChild(elChild);
 
-  const imgReflex = document.createElement("IMG");
-  imgReflex.className = "menu3CSS";
-  imgReflex.alt = data.name;
-  imgReflex.src = data.imageUrl;
+const imgReflex = document.createElement("IMG");
+imgReflex.className = "menu3CSS";
+imgReflex.alt = data.name;
+imgReflex.src = data.imageUrl;
 
-  elChild.appendChild(imgReflex);
+elChild.appendChild(imgReflex);
 
-  const nomReflex = document.createElement("p");
-  nomReflex.innerText = data.name;
-  nomReflex.className = "menu5CSS";
+const nomReflex = document.createElement("p");
+nomReflex.innerText = data.name;
+nomReflex.className = "menu5CSS";
 
-  link.appendChild(nomReflex);
+link.appendChild(nomReflex);
 
-  const prixReflex = document.createElement("p");
-  prixReflex.innerText = data.price + " €";
+const prixReflex = document.createElement("p");
+prixReflex.innerText = data.price + " €";
 
-  nomReflex.appendChild(prixReflex);
+nomReflex.appendChild(prixReflex);
 
-  return el;
+return el;
 };
 
-/*
-//Test avec demande Fetch
-fetch("http://localhost:3000/api/cameras/")
-.then(function(response) { //promesse de réponse serveur
-    if (response.ok) {
-        response.json()
-        .then(function(data) { //promesse de conversion;
-        });
-    }
-})
-.catch(function() {
-    alert("Nous avons un problème avec le serveur... Désolé")
-});
-*/
-
-//Requete au serveur
+//Requete au serveur XMLHttpRequest
 
 const xhr = new XMLHttpRequest();
 
 xhr.onreadystatechange = function () {
-  if (
-    this.readyState == XMLHttpRequest.DONE &&
-    this.status >= 200 &&
-    this.status < 300
-  ) {
-    const data = getParsedData(this.responseText);
-    data.forEach(function (d) {
-      const element = createElement(d);
-      list.appendChild(element);
-    });
-  }
+if (
+  this.readyState == XMLHttpRequest.DONE &&
+  this.status == 200
+) {
+  const data = getParsedData(this.responseText);
+  data.forEach(function (d) {
+    const element = createElement(d);
+    list.appendChild(element);
+  });
+}
 };
 
 xhr.open("GET", "http://localhost:3000/api/cameras", true);
 
 xhr.send();
+
+
+/* Méthode Fetch + GET pour aller chercher les données sur le serveur
+var reflexRequest = function (url) {
+return new Promise(function (resolve, reject) {
+  var xhr = new XMLHttpRequest();
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        resolve(
+          const data = getParsedData(this.responseText);
+          data.forEach(function (d) {
+          const element = createElement(d);
+          list.appendChild(element);
+        ));
+      } else {
+        reject(xhr);
+        // alerte si le serveur ne répond pas
+        alert("Désolé, nous avons un problème avec le serveur...")
+      };
+    };
+  };
+  xhr.open('GET','http://localhost:3000/api/cameras/', true);
+  xhr.send();
+});
+};
+*/
+/*
+fetch('http://localhost:3000/api/cameras')
+  .then(function (response) {
+    return response.json()
+  }).then(function (data) {
+    console.log(data);
+  }).then(
+      data.forEach(function (d) {
+      const element = createElement(d);
+      list.appendChild(element);
+  );
+*/
