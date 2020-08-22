ce5ef4ddbe08bc60e54f06010f26f3a07d8ba5b2
Adrien Ribeiro <adrienribeiro59200@gmail.com>
2020-08-22 20:44:35 +0200

    Maj Test unitaire Mocha
    Tentative de test unitaire sous mocha

--- test_unitaire/tests.spec.js
+++ test_unitaire/tests.spec.js
@@ -1,4 +1,4 @@
-describe('Array', function () {
+/*describe('Array', function () {
   before(function () {
     // ...
   });
@@ -21,39 +21,118 @@ describe('Array', function () {
     });
   });
 });
+*/
 
-const createElement = function (data) {
-  const el = document.createElement("DIV");
-  el.classList.add("cam");
-  el.className = "menu2CSS";
-  
-  const link = document.createElement("A");
-  link.href = "produit.html?id=" + data._id;
-  
-  el.appendChild(link);
-  
-  const elChild = document.createElement("DIV");
-  elChild.classList.add("imgContainer");
-  
-  link.appendChild(elChild);
-  
-  const imgReflex = document.createElement("IMG");
-  imgReflex.className = "menu3CSS";
-  imgReflex.alt = data.name;
-  imgReflex.src = data.imageUrl;
-  
-  elChild.appendChild(imgReflex);
-  
-  const nomReflex = document.createElement("p");
-  nomReflex.innerText = data.name;
-  nomReflex.className = "menu5CSS";
-  
-  link.appendChild(nomReflex);
-  
-  const prixReflex = document.createElement("p");
-  prixReflex.innerText = numStr(data.price) + " €";
-  
-  nomReflex.appendChild(prixReflex);
-  
-  return el;
-  };
+describe("Requête serveur"), function() { 
+  context("Si il y à 3 objets dans notre serveur", function () {
+    it("Il devrait afficher 3 elements dynamiquement", function() { 
+      const response = [
+      { name: "reflex1",
+        imageURL: "test01",
+        price: "1"
+      }, 
+      {
+        name: "reflex2",
+        imageURL: "test02",
+        price: "2"
+      }, 
+      {
+        name: "reflex3",
+        imageURL: "test03",
+        price: "3"
+      }
+    ];
+      const data = getParsedData(response);
+            data.forEach(function (d) {
+            const element = createElement(d);
+            list.appendChild(element);
+            console.log("Retour serveur: ", d);
+            });
+
+      const createElement = function (data) {
+        const el = document.createElement("DIV");
+        el.classList.add("cam");
+        el.className = "menu2CSS";
+        
+        const link = document.createElement("A");
+        link.href = "produit.html?id=" + data._id;
+        
+        el.appendChild(link);
+        
+        const elChild = document.createElement("DIV");
+        elChild.classList.add("imgContainer");
+        
+        link.appendChild(elChild);
+        
+        const imgReflex = document.createElement("IMG");
+        imgReflex.className = "menu3CSS";
+        imgReflex.alt = data.name;
+        imgReflex.src = data.imageUrl;
+        
+        elChild.appendChild(imgReflex);
+        
+        const nomReflex = document.createElement("p");
+        nomReflex.innerText = data.name;
+        nomReflex.className = "menu5CSS";
+        
+        link.appendChild(nomReflex);
+        
+        const prixReflex = document.createElement("p");
+        prixReflex.innerText = numStr(data.price) + " €";
+        
+        nomReflex.appendChild(prixReflex);
+        
+        return el;
+        };
+    });
+  });
+
+  context("Si il n'y à pas d'objet dans le serveur", function () {
+    it("Il devrait afficher aucun elements", function() { 
+      const response = {};
+      const data = getParsedData(response);
+            data.forEach(function (d) {
+            const element = createElement(d);
+            list.appendChild(element);
+            console.log("Retour serveur: ", d);
+            });
+
+      const createElement = function (data) {
+        const el = document.createElement("DIV");
+        el.classList.add("cam");
+        el.className = "menu2CSS";
+        
+        const link = document.createElement("A");
+        link.href = "produit.html?id=" + data._id;
+        
+        el.appendChild(link);
+        
+        const elChild = document.createElement("DIV");
+        elChild.classList.add("imgContainer");
+        
+        link.appendChild(elChild);
+        
+        const imgReflex = document.createElement("IMG");
+        imgReflex.className = "menu3CSS";
+        imgReflex.alt = data.name;
+        imgReflex.src = data.imageUrl;
+        
+        elChild.appendChild(imgReflex);
+        
+        const nomReflex = document.createElement("p");
+        nomReflex.innerText = data.name;
+        nomReflex.className = "menu5CSS";
+        
+        link.appendChild(nomReflex);
+        
+        const prixReflex = document.createElement("p");
+        prixReflex.innerText = numStr(data.price) + " €";
+        
+        nomReflex.appendChild(prixReflex);
+        
+        return el;
+        };
+    });
+  });
+
+};
