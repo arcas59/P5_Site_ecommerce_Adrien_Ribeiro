994f0429d5e2b5357c0ad0f75a71549b9139a608
Adrien Ribeiro <adrienribeiro59200@gmail.com>
2020-08-24 21:57:32 +0200

    Mis à jour du plan de test Mocha

---
+++ test_unitaire/testfunction.js
@@ -0,0 +1,15 @@
+// Fonction numStr pour avoir un séparateur de milier en € concernant nos prix
+function numStr(nombre, sep) {
+  nombre = '' + nombre;
+  sep = sep || ' ';
+  let c = '',
+      d = 0;
+  while (nombre.match(/^0[0-9]/)) {
+    nombre = nombre.substr(1);
+  }
+  for (let i = nombre.length-1; i >= 0; i--) {
+    c = (d != 0 && d % 3 == 0) ? nombre[i] + sep + c : nombre[i] + c;
+    d++;
+  }
+  return c;
+};
--- test_unitaire/tests.spec.js
+++ test_unitaire/tests.spec.js
@@ -1,138 +1,47 @@
-/*describe('Array', function () {
-  before(function () {
-    // ...
-  });
+let expect = chai.expect;
 
-  describe('#indexOf()', function () {
-    context('when not present', function () {
-      it('should not throw an error', function () {
-        (function () {
-          [1, 2, 3].indexOf(4);
-        }.should.not.throw());
-      });
-      it('should return -1', function () {
-        [1, 2, 3].indexOf(4).should.equal(-1);
-      });
+// Ici nous allons tester notre fonction numStr() avec differentes situations et contextes
+describe("Test sur la fonction séparateur de millier numStr()", function() {
+  context("Test avec un nombre sans espace", function() {
+    it('test avec le nombre 122000', function(){
+      let nombre = "122000";
+      expect(numStr(nombre)).to.be.equal("122 000");
     });
-    context('when present', function () {
-      it('should return the index where the element first appears in the array', function () {
-        [1, 2, 3].indexOf(3).should.equal(2);
-      });
+  });
+  context("Test avec un nombre composé de trois chiffres", function() {
+    it('test avec le nombre 200', function(){
+      let nombre = "200";
+      expect(numStr(nombre)).to.be.equal("200");
+    });
+  });
+  context("Test avec le chiffre 0", function() {
+    it('test avec le nombre 0', function(){
+      let nombre = "0";
+      expect(numStr(nombre)).to.be.equal("0");
+    });
+  });
+  context("Test avec un nombre à virgule", function() {
+    it('test avec le nombre 122000,23', function(){
+      let nombre = "122000,23";
+      expect(numStr(nombre)).to.be.equal("122 000,23");
+    });
+  });
+  context("Test avec un nombre qui possède dejà un espace", function() {
+    it('test avec le nombre 111 000', function(){
+      let nombre = "111 000";
+      expect(numStr(nombre)).to.be.equal("111 000");
+    });
+  });
+  context("Test avec un nombre qui possède un séparateur de millier composé d'un point", function() {
+    it('test avec le nombre 111.000', function(){
+      let nombre = "111.000";
+      expect(numStr(nombre)).to.be.equal("111 000");
+    });
+  });
+  context("Test avec un nombre négatif", function() {
+    it('test avec le nombre négatif -132000', function(){
+      let nombre = "-132000";
+      expect(numStr(nombre)).to.be.equal("-132 000");
     });
   });
 });
-*/
-
-describe("Requête serveur"), function() { 
-  context("Si il y à 3 objets dans notre serveur", function () {
-    it("Il devrait afficher 3 elements dynamiquement", function() { 
-      const response = [
-      { name: "reflex1",
-        imageURL: "test01",
-        price: "1"
-      }, 
-      {
-        name: "reflex2",
-        imageURL: "test02",
-        price: "2"
-      }, 
-      {
-        name: "reflex3",
-        imageURL: "test03",
-        price: "3"
-      }
-    ];
-      const data = getParsedData(response);
-            data.forEach(function (d) {
-            const element = createElement(d);
-            list.appendChild(element);
-            console.log("Retour serveur: ", d);
-            });
-
-      const createElement = function (data) {
-        const el = document.createElement("DIV");
-        el.classList.add("cam");
-        el.className = "menu2CSS";
-        
-        const link = document.createElement("A");
-        link.href = "produit.html?id=" + data._id;
-        
-        el.appendChild(link);
-        
-        const elChild = document.createElement("DIV");
-        elChild.classList.add("imgContainer");
-        
-        link.appendChild(elChild);
-        
-        const imgReflex = document.createElement("IMG");
-        imgReflex.className = "menu3CSS";
-        imgReflex.alt = data.name;
-        imgReflex.src = data.imageUrl;
-        
-        elChild.appendChild(imgReflex);
-        
-        const nomReflex = document.createElement("p");
-        nomReflex.innerText = data.name;
-        nomReflex.className = "menu5CSS";
-        
-        link.appendChild(nomReflex);
-        
-        const prixReflex = document.createElement("p");
-        prixReflex.innerText = numStr(data.price) + " €";
-        
-        nomReflex.appendChild(prixReflex);
-        
-        return el;
-        };
-    });
-  });
-
-  context("Si il n'y à pas d'objet dans le serveur", function () {
-    it("Il devrait afficher aucun elements", function() { 
-      const response = {};
-      const data = getParsedData(response);
-            data.forEach(function (d) {
-            const element = createElement(d);
-            list.appendChild(element);
-            console.log("Retour serveur: ", d);
-            });
-
-      const createElement = function (data) {
-        const el = document.createElement("DIV");
-        el.classList.add("cam");
-        el.className = "menu2CSS";
-        
-        const link = document.createElement("A");
-        link.href = "produit.html?id=" + data._id;
-        
-        el.appendChild(link);
-        
-        const elChild = document.createElement("DIV");
-        elChild.classList.add("imgContainer");
-        
-        link.appendChild(elChild);
-        
-        const imgReflex = document.createElement("IMG");
-        imgReflex.className = "menu3CSS";
-        imgReflex.alt = data.name;
-        imgReflex.src = data.imageUrl;
-        
-        elChild.appendChild(imgReflex);
-        
-        const nomReflex = document.createElement("p");
-        nomReflex.innerText = data.name;
-        nomReflex.className = "menu5CSS";
-        
-        link.appendChild(nomReflex);
-        
-        const prixReflex = document.createElement("p");
-        prixReflex.innerText = numStr(data.price) + " €";
-        
-        nomReflex.appendChild(prixReflex);
-        
-        return el;
-        };
-    });
-  });
-
-};
